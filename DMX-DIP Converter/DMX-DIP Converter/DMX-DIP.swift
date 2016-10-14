ac5c593cbfa0ef355c144f4779a75c5b385146fd
Zahzi <collinseben@gmail.com>
2016-10-14 00:09:40 -0400

    Added DMXDIPKeypadControl and DMXDIPSwitchControl classes * View is now built from these classes instead of in view controller * Changed default colors to a darker theme

--- DMX-DIP Converter/DMX-DIP Converter/DMX-DIP.swift
+++ DMX-DIP Converter/DMX-DIP Converter/DMX-DIP.swift
@@ -9,6 +9,284 @@
 import UIKit
 import Foundation
 
+import UIKit
+
+@IBDesignable class DMXDIPSwitchControl : UIControl{
+    
+    private var switches = [UIButton]()
+    private var switchLabels = [UILabel]()
+    private var arrowImg:UIImageView?
+    private var topArrowLabel = UILabel()
+    private var bottomArrowLabel = UILabel()
+    
+    
+    var onImage = UIImage()
+    var offImage = UIImage()
+    var directionalArrowImage = UIImage()
+    var topLabelText = "ON"
+    var bottomLabelText = "OFF"
+    
+    var switchLabelText:[String]?
+    var textColor = UIColor.white
+    var backColor = UIColor(hue: 0, saturation: 0, brightness: 0.2, alpha: 1.0)
+    var enableHaptics = true
+    
+    
+    var switchValues:[Bool] = []
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+       
+        for i in 0..<9{
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 16, height: 16))
+            button.addTarget(self, action: #selector(DMXDIPSwitchControl.switchFlipped), for: .touchDown)
+            button.setBackgroundImage(offImage, for: .normal)
+            switches.append(button)
+            addSubview(button)
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 16, height: 16))
+            label.textColor = textColor
+            label.text = switchLabelText != nil ? switchLabelText![i] : String(i)
+            label.textAlignment = .center
+            switchLabels.append(label)
+            addSubview(label)
+            
+            switchValues.append(false);
+        }
+        
+        arrowImg = UIImageView(frame: CGRect(x: 0, y: 0, width: 16, height: 16))
+        arrowImg?.image = directionalArrowImage
+        addSubview(arrowImg!)
+        
+        topArrowLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 16, height: 16))
+        topArrowLabel.textAlignment = .center
+        topArrowLabel.textColor = textColor
+        topArrowLabel.text = topLabelText
+        addSubview(topArrowLabel)
+        
+        bottomArrowLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 16, height: 16))
+        bottomArrowLabel.textAlignment = .center
+        bottomArrowLabel.text = bottomLabelText
+        bottomArrowLabel.textColor = textColor
+        addSubview(bottomArrowLabel)
+        
+        backgroundColor = backColor
+    }
+    
+    override func layoutSubviews() {
+        let labelHeight = 24;
+        let spacing:CGFloat = 4;
+        let switchHeight = Int(frame.size.height) - labelHeight - Int(spacing)
+        let switchWidth = Int((frame.size.width-4.0)/10) - Int(spacing)
+        
+        var switchFrame = CGRect(x: 0, y: Int(spacing), width: switchWidth, height: switchHeight)
+        var labelFrame = CGRect(x: 0, y: switchHeight + Int(spacing), width: switchWidth, height: labelHeight)
+        for (index, button) in switches.enumerated(){
+            switchFrame.origin.x = CGFloat(index) * (switchFrame.width) + 2 + CGFloat(index) * spacing
+            button.frame = switchFrame
+        }
+        for(index, label) in switchLabels.enumerated(){
+            labelFrame.origin.x = CGFloat(index) * (labelFrame.width) + 2 + CGFloat(index) * spacing
+            label.frame = labelFrame
+        }
+        
+        let arrowImgX = 9 * (labelFrame.width + spacing) - 1
+        arrowImg?.frame = CGRect(x: arrowImgX, y: CGFloat((switchHeight-64)/2), width: frame.size.width - arrowImgX, height: 64)
+        
+        var bottomArrowLabelFrame = CGRect(x: arrowImgX, y: 0, width: frame.size.width - arrowImgX, height: 24)
+        bottomArrowLabelFrame.origin.y = CGFloat(switchHeight + Int(spacing) - 24)
+        bottomArrowLabel.frame = bottomArrowLabelFrame
+        
+        topArrowLabel.frame = CGRect(x: arrowImgX, y: 0, width: frame.size.width - arrowImgX, height: 24)
+    }
+    
+    override var intrinsicContentSize:CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    func switchFlipped(sender: UIButton){
+        if(enableHaptics){
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+        let i = switches.index(of: sender)
+        switchValues[i!] = !switchValues[i!]
+        update(values: switchValues)
+    }
+    
+    func update(values: [Bool]){
+        for i in 0..<values.count{
+            let sw = switches[i]
+            if(values[i]){
+                sw.setBackgroundImage(onImage, for: .normal)
+            }else{
+                sw.setBackgroundImage(offImage, for: .normal)
+            }
+            switchValues[i] = values[i];
+        }
+        
+        sendActions(for: UIControlEvents.valueChanged)
+    }
+    
+    func updateProperties(){
+        for i in 0..<switchLabels.count{
+            switchLabels[i].textColor = textColor
+            switchLabels[i].text = switchLabelText?[i]
+        }
+        topArrowLabel.textColor = textColor
+        bottomArrowLabel.textColor = textColor
+        
+        topArrowLabel.text = topLabelText
+        bottomArrowLabel.text = bottomLabelText;
+        arrowImg?.image = directionalArrowImage
+        for i in 0..<9{
+            if(switchValues[i]){
+                switches[i].setBackgroundImage(onImage, for: .normal)
+            }else{
+                switches[i].setBackgroundImage(offImage, for: .normal)
+            }
+        }
+    }
+
+}
+
+
+@IBDesignable class DMXDIPKeypadControl: UIControl {
+    // MARK: Properties
+    
+    var buttons = [UIButton]()
+    
+    var buttonLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "", "0", "\u{232B}"]
+    var textColor = UIColor.white
+    var buttonColor = UIColor.black
+    var enableHaptics = true;
+    
+    var maxValue:Int = 511;
+    var addGroupAmnt:Int = 8;
+    
+    var value:Int = 0;
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        buttonLabels[9] = "+" + String(addGroupAmnt)
+        backgroundColor = UIColor.black
+        for i in 0..<12{
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = buttonColor
+            button.setTitle(buttonLabels[i], for: .normal)
+            button.addTarget(self, action: #selector(DMXDIPKeypadControl.buttonTapped), for: .touchDown)
+            button.layer.borderWidth = 0.5
+            button.layer.borderColor = textColor.cgColor
+            button.adjustsImageWhenHighlighted = true
+            button.adjustsImageWhenDisabled = true
+            button.showsTouchWhenHighlighted = true
+            buttons.append(button)
+            addSubview(button)
+        }
+        
+    }
+    
+    override func layoutSubviews() {
+        
+        let buttonHeight = Int(frame.size.height/4)
+        let buttonWidth = Int(frame.size.width/3)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight)
+        for(index, button) in buttons.enumerated(){
+            buttonFrame.origin.x = CGFloat(index % 3 * buttonWidth)
+            if(index % 3 == 0){
+                buttonFrame.origin.y = CGFloat(buttonHeight) * CGFloat(floor(Double(index)/3));
+            }
+            button.frame = buttonFrame
+        }
+    }
+    
+    override var intrinsicContentSize:CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    func updateProperties(){
+        buttonLabels[9] = "+" + String(addGroupAmnt)
+        for i in 0..<buttons.count{
+            buttons[i].backgroundColor = buttonColor
+            buttons[i].layer.borderColor = textColor.cgColor
+            buttons[i].setTitleColor(textColor, for: .normal)
+            buttons[i].setTitle(buttonLabels[i], for: .normal)
+        }
+    }
+    
+    func buttonTapped(sender: UIButton) {
+        if(enableHaptics){
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+        switch sender{
+        case buttons[9]: //the +n button
+            value += addGroupAmnt
+            break
+        case buttons[11]: //delete
+            var valueStr = String(value)
+            if(valueStr.characters.count > 1){
+                valueStr = valueStr.substring(to: valueStr.index(before: valueStr.endIndex))
+                value = Int(valueStr)!
+            }else{
+                value = 0
+            }
+            break
+        default:
+            let val = buttonLabels[buttons.index(of: sender)!]
+            if(value == 0){
+                value = Int(val)!;
+            }else{
+                value = Int(String(value) + (val))!
+            }
+            break
+        }
+        checkButtons(currentValue: value)
+        sendActions(for: UIControlEvents.valueChanged)
+    }
+    
+    func checkButtons(currentValue: Int){ //check to see what butons can be pressed without putting the number above what is specified; disable the rest
+        for i in 0..<buttons.count{
+            var disable:Bool = false;
+            if i == 9{ //inc
+                if currentValue + addGroupAmnt > maxValue{
+                    disable = true;
+                }
+            }else if i == 11{ //del
+                continue
+            }else{
+                if Int(buttonLabels[i]) != nil{
+                    if(Int(String(currentValue) + buttonLabels[i])! > maxValue){
+                        disable = true;
+                    }
+                }
+            }
+            if(disable){
+                buttons[i].isEnabled = false;
+                buttons[i].alpha = 0.25;
+            }else{
+                buttons[i].isEnabled = true;
+                buttons[i].alpha = 1.0;
+            }
+        }
+        
+    }
+    
+    
+}
+
+
 struct DMXDIP{
     func dipsToNum(dips:[Bool]) -> Int{ //input an array containing the values of the dips to return the integer address
         var finalNumber = 0
@@ -89,27 +367,29 @@ extension UserDefaults{
         }
         
         if(defaults.value(forKey: "btColor") == nil){
-            defaults.setColor(color: UIColor.lightText, forKey: "btColor")
+            defaults.setColor(color: UIColor.white, forKey: "btColor")
         }
         if(defaults.value(forKey: "swtColor") == nil){
-            defaults.setColor(color: UIColor.black, forKey: "swtColor")
+            defaults.setColor(color: UIColor.white, forKey: "swtColor")
         }
         if(defaults.value(forKey: "otColor") == nil){
-            defaults.setColor(color: UIColor.lightText, forKey: "otColor")
+            defaults.setColor(color: UIColor.white, forKey: "otColor")
         }
         if(defaults.value(forKey: "bColor") == nil){
-            defaults.setColor(color: UIColor(hue:0, saturation: 0, brightness: 0.26, alpha: 1.0), forKey: "bColor")
+            defaults.setColor(color: UIColor(hue:0, saturation: 0, brightness: 0.1, alpha: 1.0), forKey: "bColor")
         }
         if(defaults.value(forKey: "swColor") == nil){
-            defaults.setColor(color: UIColor(hue:0, saturation: 0, brightness: 0.60, alpha: 1.0), forKey: "swColor")
+            defaults.setColor(color: UIColor(hue:0, saturation: 0, brightness: 0.21, alpha: 1.0), forKey: "swColor")
         }
         if(defaults.value(forKey: "oColor") == nil){
-            defaults.setColor(color: UIColor(hue:0, saturation: 0, brightness: 0.26, alpha: 1.0), forKey: "oColor")
+            defaults.setColor(color: UIColor(hue:0, saturation: 0, brightness: 0.1, alpha: 1.0), forKey: "oColor")
         }
         
         if(defaults.value(forKey: "enableHaptics") == nil){
             defaults.setValue(true, forKey: "enableHaptics")
         }
+        
+        
         
     }
 }
--- DMX-DIP Converter/DMX-DIP Converter/ViewController.swift
+++ DMX-DIP Converter/DMX-DIP Converter/ViewController.swift
@@ -10,76 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var b1: UIButton!
-    @IBOutlet weak var b2: UIButton!
-    @IBOutlet weak var b3: UIButton!
-    @IBOutlet weak var b4: UIButton!
-    
-    @IBOutlet weak var b5: UIButton!
-    @IBOutlet weak var b6: UIButton!
-    @IBOutlet weak var b7: UIButton!
-    @IBOutlet weak var b8: UIButton!
-    
-    @IBOutlet weak var b9: UIButton!
-    @IBOutlet weak var b10: UIButton!
-    @IBOutlet weak var b11: UIButton!
-    @IBOutlet weak var b12: UIButton!
-    
-    @IBOutlet weak var s0: UIButton!
-    @IBOutlet weak var s1: UIButton!
-    @IBOutlet weak var s2: UIButton!
-    @IBOutlet weak var s3: UIButton!
-    @IBOutlet weak var s4: UIButton!
-    @IBOutlet weak var s5: UIButton!
-    @IBOutlet weak var s6: UIButton!
-    @IBOutlet weak var s7: UIButton!
-    @IBOutlet weak var s8: UIButton!
-    
-    @IBOutlet weak var s0t: UILabel!
-    @IBOutlet weak var s1t: UILabel!
-    @IBOutlet weak var s2t: UILabel!
-    @IBOutlet weak var s3t: UILabel!
-    @IBOutlet weak var s4t: UILabel!
-    @IBOutlet weak var s5t: UILabel!
-    @IBOutlet weak var s6t: UILabel!
-    @IBOutlet weak var s7t: UILabel!
-    @IBOutlet weak var s8t: UILabel!
-    
-    @IBOutlet weak var swView: UIView!
-    
-    
-    @IBOutlet var mainView: UIView!
     @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var switchControl: DMXDIPSwitchControl!
+    @IBOutlet weak var keypadControl: DMXDIPKeypadControl!
+    @IBOutlet var mainView: UIView!
     
-    @IBOutlet weak var topDirectionlabel: UILabel!
-    @IBOutlet weak var botDirectionlabel: UILabel!
-    @IBOutlet weak var directionArrowImage: UIImageView!
-    
-    
-    var switches:[UIButton] = [] //an array of the dip switches
-    var swLabels:[UILabel] = []
-    var buttons:[UIButton] = [] //an array holding all of the keypad buttons
-    
-    var buttonText:[String] = [] //an array holding the text that corresponds to the keypad button
-    
-    var dipArr:[Bool] = [] //holds active dips
-    
-    
-    //settings:
-    var addGroupAmnt = 16 //how much should the buttom left key increment by?
-    var invertDipDirection:Bool = false;
-    var onImage:UIImage?
-    var offImage:UIImage?
-    
+    @IBAction func switchControl(_ sender: Any) {
+        let values = switchControl.switchValues
+        let dmxValue = DMXDIP().dipsToNum(dips: values)
+        outputLabel.text = String(dmxValue)
+        keypadControl.value = dmxValue
+        keypadControl.checkButtons(currentValue: keypadControl.value)
+    }
+    @IBAction func keypadControl(_ sender: Any) {
+        outputLabel.text = String(keypadControl.value)
+        switchControl.update(values: DMXDIP().numToDips(num: keypadControl.value))
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         UserDefaults.standard.checkDefaults()
-        
-        switches = [s0,s1,s2,s3,s4,s5,s6,s7,s8]
-        buttons = [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12]
-        swLabels = [s0t, s1t, s2t, s3t, s4t, s5t, s6t, s7t, s8t]
-        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true);
@@ -92,174 +42,67 @@ class ViewController: UIViewController {
             UIApplication.shared.isIdleTimerDisabled = false
         }
         
-        if defaults.value(forKey: "invertDirection") as! Bool{
-            invertDipDirection = true;
+        if !(defaults.value(forKey: "invertDirection") as! Bool){ //normal
+            switchControl.offImage = UIImage(named: "customswitch_off.png")!
+            switchControl.onImage = UIImage(named: "customswitch_on.png")!
+            switchControl.directionalArrowImage = UIImage(named: "arrow_up_64.png")!
+            switchControl.topLabelText = "ON"
+            switchControl.bottomLabelText = "OFF"
+        }else{ //invert
+            switchControl.onImage = UIImage(named: "customswitch_off.png")!
+            switchControl.offImage = UIImage(named: "customswitch_on.png")!
+            switchControl.directionalArrowImage = UIImage(named: "arrow_down_64.png")!
+            switchControl.topLabelText = "OFF"
+            switchControl.bottomLabelText = "ON"
+        }
+        
+        keypadControl.addGroupAmnt = defaults.value(forKey: "offsetAmount")! as! Int
+        
+        if(defaults.value(forKey: "enableHaptics") as! Bool){
+            switchControl.enableHaptics = true
+            keypadControl.enableHaptics = true
         }else{
-            invertDipDirection = false;
-        }
-        addGroupAmnt = defaults.value(forKey: "offsetAmount")! as! Int
-        
-        
-        buttonText = ["+" + String(addGroupAmnt), "7", "4", "1", "0", "8", "5", "2", "\u{232B}", "9", "6", "3"] //add everything to the arrays
-        
-        if(invertDipDirection){
-            onImage = UIImage(named:  "customswitch_off.png")
-            offImage = UIImage(named: "customswitch_on.png")
-            topDirectionlabel.text = "OFF"
-            botDirectionlabel.text = "ON"
-            directionArrowImage.image = UIImage(named: "arrow_down_64")
-        }else{
-            onImage = UIImage(named:  "customswitch_on.png")
-            offImage = UIImage(named: "customswitch_off.png")
-            topDirectionlabel.text = "ON"
-            botDirectionlabel.text = "OFF"
-            directionArrowImage.image = UIImage(named: "arrow_up_64")
-        }
-        topDirectionlabel.textColor = defaults.color(forKey: "swtColor")
-        botDirectionlabel.textColor = defaults.color(forKey: "swtColor")
-        
-        for b in buttons{ //register the buttons for a touch up inside, set border/color/title
-            b.setTitleColor(defaults.color(forKey: "btColor"), for: .normal)
-            b.setTitle(buttonText[buttons.index(of: b)!], for: .normal)
-            b.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-            b.layer.borderWidth = 0.5
-            b.layer.borderColor = defaults.color(forKey: "btColor")?.cgColor
-            b.backgroundColor = defaults.color(forKey: "bColor")
+            switchControl.enableHaptics = true
+            keypadControl.enableHaptics = true
         }
         
-        dipArr.removeAll()
-        for s in switches{ //same as above, but for dips
-            s.addTarget(self, action: #selector(switchChanged), for: .touchUpInside) //touch drag inside as well?
-            s.layer.borderWidth = 3
-            s.layer.borderColor = defaults.color(forKey: "swColor")?.cgColor
-            dipArr.append(false)
-        }
-        
-        for i in 0..<swLabels.count{
-            swLabels[i].textColor = defaults.color(forKey: "swtColor")
-            
-            var val:Int = i;
-            switch defaults.value(forKey: "switchLabels") as! Int{
-            case 0:
-                val = i
-                break
-            case 1:
-                val = i + 1
-                break
-            case 2:
-                val = Int(pow(2.0, Double(i)))
-                break
-            default:
-                break
+        switch defaults.value(forKey: "switchLabels") as! Int{
+        case 1:
+            var arr:[String] = []
+            for i in 0..<9{
+                arr.append(String(i + 1))
             }
-            swLabels[i].text? = String(val);
+            switchControl.switchLabelText = arr
+            break
+        case 2:
+            var arr:[String] = []
+            for i in 0..<9{
+                arr.append(String(describing: pow(2, i)))
+            }
+            switchControl.switchLabelText = arr
+        default:
+            var arr:[String] = []
+            for i in 0..<9{
+                arr.append(String(i))
+            }
+            switchControl.switchLabelText = arr
         }
         
-        swView.backgroundColor = defaults.color(forKey: "swColor")
         mainView.backgroundColor = defaults.color(forKey: "oColor")
-        
         outputLabel.textColor = defaults.color(forKey: "otColor")
-        setDips(dips: dipArr)
+        keypadControl.buttonColor = defaults.color(forKey: "bColor")!
+        keypadControl.textColor = defaults.color(forKey: "btColor")!
+        switchControl.backgroundColor = defaults.color(forKey: "swColor")!
+        switchControl.textColor = defaults.color(forKey: "swtColor")!
         
+        switchControl.updateProperties()
+        keypadControl.updateProperties()
         
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(true)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    
-    func setDips(dips:[Bool]){ //set the dips based on a passed array
-        print("dips are ")
-        print(dips)
-        for i in 0..<dips.count{
-            let sw = switches[i]
-            if(dips[i]){
-                sw.setBackgroundImage(onImage, for: .normal)
-            }else{
-                sw.setBackgroundImage(offImage, for: .normal)
-            }
-            
-            dipArr[i] = dips[i];
-        }
-    
-    }
-    
-    func buttonTapped(sender: UIButton) { //handle the button press (for keypad)
-        selectionFeedback()
-        var value:String?
-        var out = outputLabel.text!
-        switch sender{
-        case buttons[0]: //the +n button
-            if var val = Int(out){
-                val += addGroupAmnt
-                out = String(val)
-            }else{
-                print("Error incrementing")
-            }
-            break
-        case buttons[8]: //delete
-            if(out.characters.count > 1){
-                out = out.substring(to: out.index(before: out.endIndex))
-            }else{
-                out = "0"
-            }
-            break
-        default:
-            value = buttonText[buttons.index(of: sender)!]
-            if(out == "0"){
-                out = value!
-            }else{
-                out.append(value!)
-            }
-            break
-        }
-        outputLabel.text = out;
-        checkButtons()
-        setDips(dips: DMXDIP().numToDips(num: Int(out)!))
-    }
-    
-    func checkButtons(){ //check to see what butons can be pressed without putting the number above 511; disable the rest
-        for i in 0..<buttons.count{
-            var disable:Bool = false;
-            if i == 0{
-                if var val = Int(outputLabel.text!){
-                    val += addGroupAmnt
-                    if val > 511{
-                        disable = true;
-                    }
-                }
-            }else{
-                if Int(buttonText[i]) != nil{
-                    var out = outputLabel.text!;
-                    out.append(buttonText[i])
-                    if(Int(out)! > 511){
-                        disable = true;
-                    }
-                }
-            }
-            if(disable){
-                buttons[i].isEnabled = false;
-                buttons[i].alpha = 0.25;
-            }else{
-                buttons[i].isEnabled = true;
-                buttons[i].alpha = 1.0;
-            }
-        }
-        
-    }
-    
-    func switchChanged(sender:UIButton!){ //handle the dips being pressed
-        selectionFeedback()
-        let swIndex = switches.index(of: sender)!
-        dipArr[swIndex] = !dipArr[swIndex]
-        setDips(dips: dipArr)
-        outputLabel.text = String((DMXDIP().dipsToNum(dips: dipArr)))
-        checkButtons();
-    }
-    
-    
-
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
